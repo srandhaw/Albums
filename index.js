6a8eeea59d04d4d2d6379bb91bfcd410b9aa3888
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-09-05 19:31:14 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+/** @format */
+
+import React from 'react'
+import {Text, AppRegistry} from 'react-native'
+
+const App = () =>(
+<Text>Some text</Text>
+)
+
+AppRegistry.registerComponent('albums', () => App)
